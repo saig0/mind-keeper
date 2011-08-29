c933b9e6e51661b6abc392fa07dd42c562cbbe8e
saigo <philipp.ossler@gmail.com>
2011-08-30 00:18:26 +0200

    Error und Info-Popup auf DialogBox umgestellt

--- src/de/htwk/openNoteKeeper/client/widget/InfoPopup.java
+++ src/de/htwk/openNoteKeeper/client/widget/InfoPopup.java
@@ -1,12 +1,15 @@
 package de.htwk.openNoteKeeper.client.widget;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.Constants;
-import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import de.htwk.openNoteKeeper.client.util.IconPool;
 
@@ -19,24 +22,40 @@ public class InfoPopup {
 	private final InfoPopupConstants constants = GWT
 			.create(InfoPopupConstants.class);
 
-	private final PopupPanel window = new PopupPanel(false, true);
+	private final DialogBox window = new DialogBox(false, false);
 
 	public InfoPopup(String message) {
 		window.setAnimationEnabled(true);
 		window.setGlassEnabled(true);
-		window.setTitle(constants.getTitle());
+		window.setText(constants.getTitle());
 
-		DockLayoutPanel layout = new DockLayoutPanel(Unit.EM);
+		VerticalPanel layout = new VerticalPanel();
+		layout.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
+		layout.setSpacing(5);
+
+		HorizontalPanel content = new HorizontalPanel();
+		content.setSpacing(10);
 
 		Image image = IconPool.Info_Big.createImage();
 		// image.setWidth("50px");
-		layout.addWest(image, 10);
+		content.add(image);
 
 		Label errorMessage = new Label(message);
 		// errorMessage.setWidth("*");
-		layout.add(errorMessage);
+		content.add(errorMessage);
 
-		window.add(layout);
+		Button closeButton = new Button("Schließen");
+		closeButton.addClickHandler(new ClickHandler() {
+
+			public void onClick(ClickEvent event) {
+				window.hide();
+			}
+		});
+
+		layout.add(content);
+		layout.add(closeButton);
+
+		window.setWidget(layout);
 	}
 
 	public void show() {
