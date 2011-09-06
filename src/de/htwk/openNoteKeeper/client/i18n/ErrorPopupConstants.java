1071403ea09c94cdafd0e449a7cfdaa8b2fe9f9f
saigo <saigo@localhost>
2011-09-07 00:21:33 +0200

    Internationalisierung des ErrorPopupPanels

---
+++ src/de/htwk/openNoteKeeper/client/i18n/ErrorPopupConstants.java
@@ -0,0 +1,9 @@
+package de.htwk.openNoteKeeper.client.i18n;
+
+import com.google.gwt.i18n.client.Constants;
+
+public interface ErrorPopupConstants extends Constants {
+	String title();
+
+	String close();
+}
