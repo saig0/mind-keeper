21964d73309f5b8ec14a730fb2940613ede14286
saigo <philipp.ossler@gmail.com>
2012-03-04 17:36:05 +0100

    Note-Widget

--- src/de/htwk/openNoteKeeper/client/note/view/whiteboard/SingleNoteViewImpl.java
+++ src/de/htwk/openNoteKeeper/client/note/view/whiteboard/SingleNoteViewImpl.java
@@ -2,15 +2,16 @@ package de.htwk.openNoteKeeper.client.note.view.whiteboard;
 
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
 import de.htwk.openNoteKeeper.client.note.presenter.whiteboard.SingleNotePresenter.SingleNoteView;
-import de.htwk.openNoteKeeper.client.util.IconPool;
 
 public class SingleNoteViewImpl implements SingleNoteView {
 
 	private final Widget main;
 	private Image backgroundImage;
+	private Label titleLabel;
 
 	public SingleNoteViewImpl() {
 		main = createLayout();
@@ -18,9 +19,15 @@ public class SingleNoteViewImpl implements SingleNoteView {
 
 	private Widget createLayout() {
 		HorizontalPanel panel = new HorizontalPanel();
+		panel.setSpacing(5);
+		panel.addStyleName("note");
 
-		backgroundImage = IconPool.PostIt.createImage();
-		panel.add(backgroundImage);
+		// backgroundImage = IconPool.PostIt.createImage();
+		// panel.add(backgroundImage);
+
+		titleLabel = new Label("");
+		titleLabel.addStyleName("noteHeader");
+		panel.add(titleLabel);
 
 		return panel;
 	}
@@ -31,6 +38,15 @@ public class SingleNoteViewImpl implements SingleNoteView {
 
 	public void setSize(int width, int height) {
 		main.setSize(width + "px", height + "px");
-		backgroundImage.setSize(width + "px", height + "px");
+		// backgroundImage.setSize(width + "px", height + "px");
+	}
+
+	public void setTitle(String title) {
+		titleLabel.setText(title);
+	}
+
+	public void setContent(String content) {
+		// TODO Auto-generated method stub
+
 	}
 }
