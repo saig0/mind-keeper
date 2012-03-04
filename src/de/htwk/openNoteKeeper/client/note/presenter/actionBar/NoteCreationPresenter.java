d208439dccd6e20e8bedb42a9cbca577df025cc3
saigo <saigo@localhost>
2012-03-04 22:33:06 +0100

    Bugfix: Erstellung der Notes und ?bernehmen der ?nderung im Modell

--- src/de/htwk/openNoteKeeper/client/note/presenter/actionBar/NoteCreationPresenter.java
+++ src/de/htwk/openNoteKeeper/client/note/presenter/actionBar/NoteCreationPresenter.java
@@ -3,11 +3,13 @@ package de.htwk.openNoteKeeper.client.note.presenter.actionBar;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.inject.Inject;
 import com.mvp4g.client.annotation.Presenter;
 import com.mvp4g.client.presenter.BasePresenter;
 
+import de.htwk.openNoteKeeper.client.main.presenter.Session;
 import de.htwk.openNoteKeeper.client.note.NoteEventBus;
 import de.htwk.openNoteKeeper.client.note.service.NoteServiceAsync;
 import de.htwk.openNoteKeeper.client.note.view.actionBar.NoteCreationViewImpl;
@@ -45,9 +47,11 @@ public class NoteCreationPresenter extends
 				String nodeName = view.getName();
 				if (!nodeName.isEmpty()) {
 					// TODO position bestimmen
-					CoordinateDTO position = new CoordinateDTO(
-							100 + (int) (Math.random() * 100),
-							100 + (int) (Math.random() * 100));
+					int left = (Window.getClientWidth() / 2)
+							+ -(int) (Math.random() * 100);
+					int top = (Window.getClientHeight() / 2)
+							+ (int) (Math.random() * 100);
+					CoordinateDTO position = new CoordinateDTO(left, top);
 					CoordinateDTO size = new CoordinateDTO(200, 200);
 
 					noteService.createNote(activeWhiteBoard.getKey(), nodeName,
@@ -58,6 +62,8 @@ public class NoteCreationPresenter extends
 								protected void success(NoteDTO note) {
 									view.hide();
 									eventBus.showNote(note);
+									// TODO Modell auf Client aktualliesieren
+									eventBus.loggedIn(Session.getCurrentUser());
 								}
 							});
 				}
