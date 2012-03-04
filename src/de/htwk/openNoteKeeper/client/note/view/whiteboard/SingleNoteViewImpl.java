e3fc66192d10ab5ff7a8ebb2020e8b7430f6d312
saigo <philipp.ossler@gmail.com>
2012-03-04 19:21:28 +0100

    Vorbereitung auf Context-Menu

--- src/de/htwk/openNoteKeeper/client/note/view/whiteboard/SingleNoteViewImpl.java
+++ src/de/htwk/openNoteKeeper/client/note/view/whiteboard/SingleNoteViewImpl.java
@@ -2,6 +2,8 @@ package de.htwk.openNoteKeeper.client.note.view.whiteboard;
 
 import com.google.gwt.event.dom.client.BlurEvent;
 import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.FocusEvent;
 import com.google.gwt.event.dom.client.FocusHandler;
 import com.google.gwt.event.dom.client.MouseOutEvent;
@@ -9,17 +11,20 @@ import com.google.gwt.event.dom.client.MouseOutHandler;
 import com.google.gwt.event.dom.client.MouseOverEvent;
 import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.user.client.ui.FocusPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import de.htwk.openNoteKeeper.client.note.presenter.whiteboard.SingleNotePresenter.SingleNoteView;
+import de.htwk.openNoteKeeper.client.util.IconPool;
 
 public class SingleNoteViewImpl implements SingleNoteView {
 
 	private final Widget main;
-	private Image backgroundImage;
 	private Label titleLabel;
 	private boolean isSelected = false;
 
@@ -31,16 +36,40 @@ public class SingleNoteViewImpl implements SingleNoteView {
 		final FocusPanel main = new FocusPanel();
 		main.addStyleName("note");
 
-		HorizontalPanel panel = new HorizontalPanel();
+		VerticalPanel panel = new VerticalPanel();
 		panel.setSize("100%", "100%");
 		panel.setSpacing(5);
 
-		// backgroundImage = IconPool.PostIt.createImage();
-		// panel.add(backgroundImage);
+		HorizontalPanel header = new HorizontalPanel();
+		header.setSize("100%", "100%");
 
 		titleLabel = new Label("");
 		titleLabel.addStyleName("noteHeader");
-		panel.add(titleLabel);
+		header.add(titleLabel);
+
+		Image contextImage = IconPool.Settings_Small.createImage();
+		contextImage.addStyleName("clickable");
+
+		contextImage.addClickHandler(new ClickHandler() {
+
+			public void onClick(ClickEvent event) {
+				PopupPanel popup = new PopupPanel(true, false);
+				popup.addStyleName("top");
+
+				HorizontalPanel content = new HorizontalPanel();
+				content.add(new Label("context menu coming soon..."));
+				popup.setWidget(content);
+
+				popup.setPopupPosition(event.getClientX(), event.getClientY());
+				popup.show();
+			}
+		});
+
+		header.add(contextImage);
+		header.setCellHorizontalAlignment(contextImage,
+				HasHorizontalAlignment.ALIGN_RIGHT);
+
+		panel.add(header);
 		main.setWidget(panel);
 
 		main.addMouseOverHandler(new MouseOverHandler() {
@@ -84,7 +113,6 @@ public class SingleNoteViewImpl implements SingleNoteView {
 
 	public void setSize(int width, int height) {
 		main.setSize(width + "px", height + "px");
-		// backgroundImage.setSize(width + "px", height + "px");
 	}
 
 	public void setTitle(String title) {
