75b3a0c172891b474faf29e2a150cc1af8c54aca
saigo <philipp.ossler@gmail.com>
2012-04-18 20:43:37 +0200

    Warnung f?r nicht optimierte Browser

--- src/de/htwk/openNoteKeeper/client/main/presenter/MainPresenter.java
+++ src/de/htwk/openNoteKeeper/client/main/presenter/MainPresenter.java
@@ -1,12 +1,16 @@
 package de.htwk.openNoteKeeper.client.main.presenter;
 
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
 import com.mvp4g.client.annotation.Presenter;
 import com.mvp4g.client.presenter.BasePresenter;
 
 import de.htwk.openNoteKeeper.client.main.MainEventBus;
 import de.htwk.openNoteKeeper.client.main.view.MainViewImpl;
+import de.htwk.openNoteKeeper.client.widget.StatusArea;
+import de.htwk.openNoteKeeper.client.widget.StatusPanel;
 import de.htwk.openNoteKeeper.shared.UserDTO;
 
 @Presenter(view = MainViewImpl.class)
@@ -17,12 +21,34 @@ public class MainPresenter extends BasePresenter<MainViewImpl, MainEventBus> {
 		public void setActionBar(Widget actionWidget);
 	}
 
+	@Inject
+	private StatusArea statusArea;
+
 	@Override
 	public void bind() {
 
 	}
 
 	public void onStart() {
+		String userAgent = getUserAgent();
+		if (userAgent.contains("chrome")) {
+			// ok
+		} else if (userAgent.contains("firefox")) {
+			// TODO für Firefox optimieren
+			statusArea
+					.addStatusMessage(new StatusPanel(
+							"optimale Darstellung der Website mit Chrome-Browser (Firefox folgt)",
+							true));
+		} else {
+			statusArea
+					.addStatusMessage(new StatusPanel(
+							"optimale Darstellung der Website mit Chrome-Browser",
+							true));
+		}
+	}
+
+	private String getUserAgent() {
+		return Window.Navigator.getUserAgent().toLowerCase();
 	}
 
 	public void onLoggedIn(UserDTO user) {
